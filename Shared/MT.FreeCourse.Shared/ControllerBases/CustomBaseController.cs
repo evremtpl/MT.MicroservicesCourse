c992d1eb76f6c796fa6021730799e59f1275aebe
merve topal <merve.tpl@WINDOWSLIVE.COM>
2023-03-21 21:42:32 +0300

    CourseController and CustomBaseController Added

---
+++ Shared/MT.FreeCourse.Shared/ControllerBases/CustomBaseController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MT.FreeCourse.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MT.FreeCourse.Shared.ControllerBases
+{
+    public class CustomBaseController  :ControllerBase
+    {
+
+        public IActionResult CreateActionResultInstance<T>(Response<T> response)
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.StatusCode,
+            };
+        }
+    }
+}
