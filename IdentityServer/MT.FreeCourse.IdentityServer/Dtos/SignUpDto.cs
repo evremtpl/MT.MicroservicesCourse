4fcdfaa23f1667e49b7cfa51775cbea52d655278
merve topal <merve.tpl@WINDOWSLIVE.COM>
2023-03-22 22:33:00 +0300

    identityserver user operation

---
+++ IdentityServer/MT.FreeCourse.IdentityServer/Dtos/SignUpDto.cs
@@ -0,0 +1,12 @@
+﻿namespace MT.FreeCourse.IdentityServer.Dtos
+{
+    public class SignUpDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserName { get; set; }
+
+        public string City { get; set; }
+
+    }
+}
