edba5eafa4bba2ff33022a9b3838bae25198b357
merve topal <merve.tpl@WINDOWSLIVE.COM>
2023-03-27 22:42:02 +0300

    IBasketService Interface was created

---
+++ Services/Basket/MT.FreeCourse.Services.Basket/Services/IBasketService.cs
@@ -0,0 +1,12 @@
+﻿using MT.FreeCourse.Services.Basket.Dtos;
+using MT.FreeCourse.Shared.Dtos;
+
+namespace MT.FreeCourse.Services.Basket.Services
+{
+    public interface IBasketService
+    {
+        public Task<Response<BasketDto>> GetBasket(string userId);
+        public Task<Response<bool>> SaveOrUpdate(BasketDto basketDto);
+        public Task<Response<bool>> Delete(string userId);
+    }
+}
